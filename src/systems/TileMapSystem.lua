1f9133ee94d18d46b794c426bb652849c8a3957c
Robert Sigler <sigle023@umn.edu>
2016-08-20 13:23:10 -0500

    Fix town map

--- src/systems/TileMapSystem.lua
+++
@@ -1,9 +0,0 @@
-local TileMapSystem = tiny.processingSystem()
-
-TileMapSystem.filter = tiny.requireAll("tileMap")
-
-function TileMapSystem:process(e, dt)
-    local tileMap = e.tileMap
-end
-
-return TileMapSystem
