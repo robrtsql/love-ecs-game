dba731970379fea3173a20a806a911670fe75415
Robert Sigler <sigle023@umn.edu>
2016-09-09 01:46:55 -0500

    Add second town map and teleporter colliders
    These will be used to send player to a different map when
    they move to the edge of the screen.

---
+++ assets/maps/town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="town" tilewidth="16" tileheight="16" tilecount="1472" columns="46">
+ <image source="../tilesets/towntileset.png" width="736" height="512"/>
+</tileset>
